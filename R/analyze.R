1113b0f5fdadb3b9d6b309009c7d851cf82b8fca
Wanding Zhou <zhouw3@email.chop.edu>
2021-03-29 22:53:27 -0400

    added testEnrichment

--- R/analyze.R
+++ R/analyze.R
@@ -1,10 +1,20 @@
 
 #' test whether query is significantly enriched in database
-enrichmentTest1 = function(querySet, databaseSet, universeSet) {
-} 
+testEnrichment = function(setQ, setD, setU) {
+    mtx = matrix(c(
+        length(intersect(setQ, setD)),
+        length(setdiff(setD, setQ)), 
+        length(setdiff(setQ, setD)), 
+        length(setdiff(setU, union(setD, setQ)))),
+        nrow = 2, 
+        dimnames = list(
+            Query = c("Q_in","Q_out"),
+            Database = c("D_in","D_out")))
+    list(mtx = mtx, test = fisher.test(mtx))
+}
 
 #' test all databaseSet and return a list ranked by enrichment (odds-ratio)
-enrichmentTest = function() {
+testEnrichmentAll = function() {
 }
 
 
