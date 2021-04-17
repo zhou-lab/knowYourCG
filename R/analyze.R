3ef1e4fe79f83c01677412188ce29dd9dabb2ea7
Fred Dong <fdong2@gmail.com>
2021-04-17 02:06:46 -0400

    first attempt at testEnrichmentAll for categorical

--- R/analyze.R
+++ R/analyze.R
@@ -13,15 +13,53 @@ testEnrichment = function(setQ, setD, setU) {
     list(mtx = mtx, test = fisher.test(mtx))
 }
 
+testCategoricalFisher <- function(database, ProbeIDs, sigProbes) {
+    categories <- unique(database)
+    result <- sapply(
+        categories,
+        function(category) {
+            setD <- probeIDs[database == category]
+            fishertest <- testEnrichment(
+                setQ = sigProbes,
+                setD = setD,
+                setU = probeIDs
+            )
+            OR <- c(fishertest$test$estimate)
+            names(OR) <- NULL
+            return(OR)
+        }
+    )
+    return(result)
+}
+
 #' test all databaseSet and return a list ranked by enrichment (odds-ratio)
-testEnrichmentAll = function(experiment, databaseSets = NULL, percTop = 25, sig.threshold = 1e-8) {
+testEnrichmentAll = function(probeIDs, pVals, databaseSets = NULL, percTop = 25, sig.threshold = 1e-8) {
+    # assume same index for results and databases
+    # get significant probes. probes and pVals must be in same order
+    sigProbes <- probeIDs[pvals <= sig.threshold]
+    # calculate number of databases to keep
+    nDatabases <- ceiling((percTop/100)*length(databaseSets))
 	# prioritize database sets by proportion of probes in dataset that overlap with significant hits
+    # then take top percTop % database sets
+    # note: databases read in from tbk files are named vectors
+    topDatabases <- databases[
+        order(
+            lapply(
+                names(databases),
+                function(dbName) sum(sigProbes %in% databases[[dbName]]$ProbeID)
+            ),
+            decreasing = TRUE
+        )
+    ][1:nDatabases]
     
-    # take top percTop % database sets
+    # apply enrichment tests and get test results
+    # TODO: need to name database list with database names? Otherwise can't know which is which
+    results <- unlist(lapply(topDatabases, testCategoricalFisher, ProbeIDs = ProbeIDs, sigProbes = sigProbes))
     
-    # apply enrichment tests and get scores
+    # sort results by output
+    results <- sort(results, decreasing = TRUE)
     
-    # continuous variable stuff
+    # TODO: handle continuous value databases
 }
 
 
