541f2c8169de8201b5e7d76decb1b4a06ffce786
ethan <57536747+ethanmoyer@users.noreply.github.com>
2021-07-05 09:37:04 -0400

    20210705

--- R/20210626_EM_analyze.R
+++ R/20210626_EM_analyze.R
@@ -5,6 +5,10 @@
 #' @param databaseSet Vector of probes corresponding to a single database set of interest.
 #' @param universeSet Vector of probes in the universe set containing all of 
 #' the probes to be considered in the test.
+#' @param estimate.type String indicating the estimate to report. (Default: 
+#' "ES")
+#' @param p.pvalue.adj Logical value indicating whether to report the adjusted 
+#' p-value. (Default: FALSE)
 #' @param verbose Logical value indicating whether to display intermediate 
 #' text output about the type of test. Optional. (Default: FALSE)
 #'
@@ -14,7 +18,7 @@
 #' @import fgsea
 #'
 #' @export
-testEnrichment1 = function(querySet, databaseSet, universeSet, p.pvalue.adj=FALSE, estimate.type="ES", verbose=FALSE) {
+testEnrichment1 = function(querySet, databaseSet, universeSet, estimate.type="ES", pvalue.adj=FALSE, verbose=FALSE) {
     if (is.numeric(querySet)) { # a named vector of continuous value
         if(is.numeric(databaseSet)) { # numeric db
             if (verbose) {
@@ -36,7 +40,7 @@ testEnrichment1 = function(querySet, databaseSet, universeSet, p.pvalue.adj=FALS
             results = testEnrichmentFGSEA(
                 querySet=querySet,
                 databaseSet=databaseSet, 
-                p.pvalue.adj=p.pvalue.adj, 
+                pvalue.adj=pvalue.adj, 
                 estimate.type=estimate.type)
         }
     } else { # categorical query
@@ -51,7 +55,7 @@ testEnrichment1 = function(querySet, databaseSet, universeSet, p.pvalue.adj=FALS
             results = testEnrichmentFGSEA(
                 querySet=databaseSet, 
                 databaseSet=querySet, 
-                p.pvalue.adj=p.pvalue.adj,
+                pvalue.adj=pvalue.adj,
                 estimate.type=estimate.type)
         } else { # categorical db
             if (verbose) {
@@ -69,33 +73,55 @@ testEnrichment1 = function(querySet, databaseSet, universeSet, p.pvalue.adj=FALS
     return(results)
 }
 
+inferPlatformFromProbeIDs <- function(probeIDs) {
+    sig = get(load(url("https://zhouserver.research.chop.edu/sesameData/probeIDSignature.rda")))$probeID
+    names(which.max(vapply(
+        sig, function(x) sum(probeIDs %in% x), integer(1))))
+}
+
 
 #' testEnrichmentAll tests for the enrichment of set of probes (query set) in a number of features (database sets).
 #'
 #' @param querySet Vector of probes of interest (e.g., significant probes)
 #' @param databaseSets List of vectors of probes corresponding to multiple 
-#' database sets. Optional. (Default: NULL)
+#' database sets. Optional. (Default: NA)
+#' @param universeSet Vector of probes in the universe set containing all of 
+#' the probes to be considered in the test. If it is not provided, it will be 
+#' inferred from the provided platform. (Default: NA)
 #' @param platform String corresponding to the type of platform to use. Either 
-#' MM285, EPIC, HM450, or HM27.
+#' MM285, EPIC, HM450, or HM27. If it is not provided, it will be inferred 
+#' from the query set probeIDs (Default: NA)
+#' @param estimate.type String indicating the estimate to report. (Default: 
+#' "ES")
+#' @param p.pvalue.adj Logical value indicating whether to report the adjusted 
+#' p-value. (Default: FALSE)
 #' @param verbose Logical value indicating whether to display intermediate 
 #' text output about the type of test. Optional. (Default: FALSE)
 #'
 #' @return One list containing features corresponding the test estimate, p-value, and type of test.
 #'
 #' @export
-testEnrichmentAll = function(querySet, databaseSets=NULL, platform = c("MM285", "EPIC", "HM450", "HM27"), p.pvalue.adj=FALSE, estimate.type="ES", verbose=FALSE) {
+testEnrichmentAll = function(querySet, databaseSets=NA, universeSet=NA, platform=NA, estimate.type="ES", pvalue.adj=FALSE, verbose=FALSE) {
     options(timeout=1000)
-    if (platform == "MM285") {
-        universeSet = get(load(url("http://zhouserver.research.chop.edu/sesameData/MM285.mm10.manifest.rda")))$probeID
-    } else if (platform == "EPIC") {
-        universeSet = readRDS(url("http://zhouserver.research.chop.edu/InfiniumAnnotation/current/EPIC/EPIC.hg19.manifest.rds"))$probeID
-    } else if (platform == "HM450") {
-        universeSet = readRDS(url("http://zhouserver.research.chop.edu/InfiniumAnnotation/current/HM450/HM450.hg19.manifest.rds"))$probeID
-    } else if (platform == "HM27") {
-        universeSet = readRDS(url("http://zhouserver.research.chop.edu/InfiniumAnnotation/current/HM27/HM27.hg19.manifest.rds"))$probeID
+
+    if (is.na(universeSet)) {
+        print("The universeSet was not defined. Loading in universeSet based on platform.")
+        if (is.na(platform)) {
+            print("The platform was not defined. Inferring platform from probeIDs.")
+            platform = inferPlatformFromProbeIDs(querySet)
+        }
+        if (platform == "MM285") {
+            universeSet = get(load(url("http://zhouserver.research.chop.edu/sesameData/MM285.mm10.manifest.rda")))$Probe_ID
+        } else if (platform == "EPIC") {
+            universeSet = readRDS(url("http://zhouserver.research.chop.edu/InfiniumAnnotation/current/EPIC/EPIC.hg19.manifest.rds"))$probeID
+        } else if (platform == "HM450") {
+            universeSet = readRDS(url("http://zhouserver.research.chop.edu/InfiniumAnnotation/current/HM450/HM450.hg19.manifest.rds"))$probeID
+        } else if (platform == "HM27") {
+            universeSet = readRDS(url("http://zhouserver.research.chop.edu/InfiniumAnnotation/current/HM27/HM27.hg19.manifest.rds"))$probeID
+        }
     }
 
-    if (is.null(databaseSets)) {
+    if (is.na(databaseSets)) {
         print("Database set was not defined. Loading in default database sets of transcription factor binding sites.")
         databaseSets = readRDS(url("http://zhouserver.research.chop.edu/kyCG/20210601_MM285_TFBS_ENCODE.rds"))
     }
@@ -106,16 +132,13 @@ testEnrichmentAll = function(querySet, databaseSets=NULL, platform = c("MM285", 
                                     querySet=querySet, 
                                     databaseSet=databaseSet, 
                                     universeSet=universeSet, 
-                                    p.pvalue.adj=p.pvalue.adj,
+                                    pvalue.adj=pvalue.adj,
                                     estimate.type=estimate.type, 
                                     verbose=verbose)
             ))
     results = results[order(results$p.value, decreasing=F), ]
     return(results)
     ## apply multi-test correction, BH, FDR
-
-
-
 }
 
 
@@ -170,6 +193,10 @@ calcFoldChange = function(mtx){
 #' @param querySet Vector of probes of interest (e.g., significant probes)
 #' @param databaseSet Vector of probes corresponding to a single database set 
 #' of interest.
+#' @param p.pvalue.adj Logical value indicating whether to report the adjusted 
+#' p-value. (Default: FALSE)
+#' @param estimate.type String indicating the estimate to report. (Default: 
+#' "ES")
 #'
 #' @return A DataFrame with the estimate/statistic, p-value, and name of test 
 #' for the given results.
