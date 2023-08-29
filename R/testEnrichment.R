cd40cf11727f0b256be143b034f30620fe8aeb7c
Goldberg <golddc72@pennmedicine.upenn.edu>
2023-08-29 11:23:08 -0400

    update testEnrichment,vigentte

--- R/testEnrichment.R
+++ R/testEnrichment.R
@@ -9,11 +9,12 @@
 #' the probes to be considered in the test. If it is not provided, it will be
 #' inferred from the provided platform. (Default: NA).
 #' @param alternative "two.sided", "greater", or "less"
+#' @param include_genes include gene link enrichment testing
 #' @param platform String corresponding to the type of platform to use. Either
 #' MM285, EPIC, HM450, or HM27. If it is not provided, it will be inferred
 #' from the query set probeIDs (Default: NA).
 #' @param silent output message? (Default: FALSE)
-#' @return One list containing features corresponding the test estimate,
+#' @return A data frame containing features corresponding to the test estimate,
 #' p-value, and type of test.
 #' @importFrom dplyr bind_rows
 #' @examples
@@ -27,19 +28,23 @@
 #' @export
 testEnrichment <- function(
         query, databases = NULL, universe = NULL, alternative = "greater",
-        platform = NULL, silent = FALSE) {
+        include_genes = FALSE, platform = NULL, silent = FALSE) {
 
     platform <- queryCheckPlatform(platform, query, silent = silent)
 
     if (is.null(databases)) {
         dbs <- c(KYCG_getDBs(KYCG_listDBGroups( # by default, all dbs + gene
-            platform, type="categorical")$Title, silent = silent),
-            KYCG_buildGeneDBs(query, platform, silent = silent))
+            platform, type="categorical")$Title, silent = silent))
     } else if (is.character(databases)) {
         dbs <- KYCG_getDBs(databases, platform = platform, silent = silent)
     } else {
         dbs <- databases
     }
+
+    if (include_genes) {
+        dbs <- c(dbs, KYCG_buildGeneDBs(query, platform, silent = silent))
+    }
+
     ## there shouldn't be empty databases, but just in case
     dbs <- dbs[vapply(dbs, length, integer(1)) > 0]
     if (!silent) {
@@ -167,5 +172,61 @@ testEnrichmentFisherN <- function(
 }
 
 
+
+#' build gene-probe association database
+#'
+#' @param query the query probe list. If NULL, use all the probes
+#' on the platform
+#' @param platform HM450, EPIC, MM285, Mammal40, will infer from
+#' query if not given
+#' @param genome hg38, mm10, ..., will infer if not given.
+#' For additional mapping, download the GRanges object from
+#' http://zwdzwd.github.io/InfiniumAnnotation
+#' and provide the following argument
+#' ..., genome = sesameAnno_buildManifestGRanges("downloaded_file"),...
+#' to this function.
+#' @param max_distance probe-gene distance for association
+#' @param silent suppress messages
+#' @return gene databases
+#' @import sesameData
+#' @importFrom GenomicRanges findOverlaps
+#' @importFrom IRanges subsetByOverlaps
+#' @importFrom S4Vectors subjectHits
+#' @importFrom S4Vectors queryHits
+#' @examples
+#' query <- c("cg04707299", "cg13380562", "cg00480749")
+#' dbs <- KYCG_buildGeneDBs(query, platform = "EPIC")
+#' testEnrichment(query, dbs, platform = "EPIC")
+#' @export
+KYCG_buildGeneDBs <- function(
+        query = NULL, platform = NULL,
+        genome = NULL, max_distance = 10000, silent = FALSE) {
+
+    platform <- queryCheckPlatform(platform, query, silent = silent)
+    genes <- sesameData_txnToGeneGRanges(
+        sesameData_getTxnGRanges(
+            sesameData_check_genome(NULL, platform)))
+    all_probes <- sesameData_getManifestGRanges(platform, genome = genome)
+    if (!is.null(query)) {
+        probes <- all_probes[names(all_probes) %in% query] }
+
+    ## skip non-overlapping genes, strand always ignored
+    genes <- subsetByOverlaps(
+        genes, probes + max_distance, ignore.strand = TRUE)
+    hits <- findOverlaps(
+        genes, all_probes + max_distance, ignore.strand = TRUE)
+    dbs <- split(names(all_probes)[subjectHits(hits)],
+                 names(genes)[queryHits(hits)])
+    gene_names <- genes[names(dbs)]$gene_name
+    res <- lapply(seq_along(dbs), function(i) {
+        d1 <- dbs[[i]];
+        attr(d1, "group") <- sprintf("KYCG.%s.gene.00000000", platform);
+        attr(d1, "dbname") <- names(dbs)[i];
+        attr(d1, "gene_name") <- gene_names[i];
+        d1;})
+    names(res) <- names(dbs)
+    message(sprintf("Building %d gene DBs for %s...", length(res), platform))
+    res
+}
 
 
