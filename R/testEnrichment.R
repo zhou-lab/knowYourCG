c87aa9baf991ca8b73664188150e82b3f4e17d45
Wanding Zhou <zhouwanding@gmail.com>
2026-01-03 09:13:04 -0500

    refactored testEnrichment

--- R/testEnrichment.R
+++ R/testEnrichment.R
@@ -1,189 +1,306 @@
-#' testEnrichment tests for the enrichment of query in knowledgebase sets
+#' Test for enrichment of query in knowledgebase sets
 #'
-#' @param query For array input, it is a vector of probes of interest
+#' @param query For array input, a vector of probes of interest
 #' (e.g., significant differential methylated probes). For sequencing data
-#' input, it expect the file name for YAME-compressed CG sets.
+#' input, the file name for YAME-compressed CG sets.
 #' @param databases List of vectors corresponding to the database sets of
 #' interest with associated meta data as an attribute to each element.
-#' Optional. (Default: NA)
-#' @param universe Vector of probes in the universe set containing all of
-#' the probes to be considered in the test. If it is not provided, it will be
-#' inferred from the provided platform. (Default: NA).
-#' @param alternative "two.sided", "greater", or "less"
-#' @param include_genes include gene link enrichment testing
-#' @param platform String corresponding to the type of platform to use. Either
-#' MM285, EPIC, HM450, or HM27. If it is not provided, it will be inferred
-#' from the query set probeIDs (Default: NA).
-#' @param silent output message? (Default: FALSE)
-#' @param mtc_by_group peform multiple testing correction within 
-#' knowledgebase groups (Default: TRUE)
-#' @param mtc_method method for multiple test correction (default: fdr)
+#' If NULL, all available databases for the platform are used. (Default: NULL)
+#' @param universe Vector of probes in the universe set containing all
+#' probes to be considered in the test. If NULL, will be inferred from the
+#' provided platform. (Default: NULL)
+#' @param alternative Test alternative: "two.sided", "greater", or "less".
+#' (Default: "greater")
+#' @param include_genes Include gene link enrichment testing. (Default: FALSE)
+#' @param platform String corresponding to the type of platform to use:
+#' MM285, EPIC, HM450, or HM27. If NULL, will be inferred from query set
+#' probe IDs. (Default: NULL)
+#' @param silent Suppress output messages? (Default: FALSE)
+#' @param mtc_by_group Perform multiple testing correction within 
+#' knowledgebase groups. (Default: TRUE)
+#' @param mtc_method Method for multiple test correction. (Default: "fdr")
 #' @return A data frame containing features corresponding to the test estimate,
-#' p-value, and type of test.
+#' p-value, and type of test, ordered by significance.
 #' @importFrom dplyr bind_rows
 #' @examples
 #'
 #' library(SummarizedExperiment)
 #' library(sesameData)
 #' library(knowYourCG)
-#' kycgDataCache(data_titles="KYCG.MM285.chromHMM.20210210")
+#' kycgDataCache(data_titles = "KYCG.MM285.chromHMM.20210210")
 #' sesameDataCache("MM285.tissueSignature")
 #' df <- rowData(sesameDataGet("MM285.tissueSignature"))
 #' probes <- df$Probe_ID[df$branch == "B_cell"]
-#' res <- testEnrichment(probes, "chromHMM", platform="MM285")
+#' res <- testEnrichment(probes, "chromHMM", platform = "MM285")
 #' 
 #' \dontrun{
 #' # Define temporary directory and file URLs
 #' temp_dir <- tempdir()
 #' knowledgebase <- file.path(temp_dir, "ChromHMM.20220414.cm")
 #' query <- file.path(temp_dir, "mm10_f3_10cells.cg")
+#' 
 #' # URLs for the knowledgebase and query files
-#' knowledgebase_url <- "https://github.com/zhou-lab/KYCGKB_mm10/raw/refs/heads/main/ChromHMM.20220414.cm"
-#' query_url <- "https://github.com/zhou-lab/YAME_test/raw/refs/heads/main/input/cx/mm10_f3_10cells.cg"
+#' knowledgebase_url <- paste0(
+#'   "https://github.com/zhou-lab/KYCGKB_mm10/raw/refs/heads/main/",
+#'   "ChromHMM.20220414.cm"
+#' )
+#' query_url <- paste0(
+#'   "https://github.com/zhou-lab/YAME_test/raw/refs/heads/main/",
+#'   "input/cx/mm10_f3_10cells.cg"
+#' )
+#' 
 #' # Download the files
 #' download.file(knowledgebase_url, destfile = knowledgebase)
 #' download.file(query_url, destfile = query)
+#' 
 #' # Confirm file download
 #' list.files(temp_dir)
-#' res = testEnrichment(query, knowledgebase)
+#' res <- testEnrichment(query, knowledgebase)
 #' }
 #' @export
 testEnrichment <- function(
-    query, databases = NULL, universe = NULL, alternative = "greater",
-    include_genes = FALSE, platform = NULL, silent = FALSE,
-    mtc_by_group = TRUE, mtc_method = "fdr") {
-
-    if (length(query) == 1 && !grepl("^c[gh]", query) &&
-        !grepl("^rs", query) && is.null(platform)) {
-        res <- testEnrichment2(query, databases, universe_fn = universe,
-            alternative = alternative)
+    query, 
+    databases = NULL, 
+    universe = NULL, 
+    alternative = "greater",
+    include_genes = FALSE, 
+    platform = NULL, 
+    silent = FALSE,
+    mtc_by_group = TRUE, 
+    mtc_method = "fdr") {
+    
+    ## Validate alternative parameter
+    alternative <- match.arg(alternative, c("greater", "less", "two.sided"))
+    
+    ## Check if query is a file path for sequencing data
+    is_seq_data <- length(query) == 1 && 
+                   !grepl("^c[gh]", query) &&
+                   !grepl("^rs", query) && 
+                   is.null(platform)
+    
+    if (is_seq_data) {
+        res <- testEnrichment2(
+            query, 
+            databases, 
+            universe_fn = universe,
+            alternative = alternative
+        )
     } else {
-        platform <- queryCheckPlatform(platform, query, silent = silent)
-        if (is.null(databases)) {
-            dbs <- c(getDBs(listDBGroups( # by default, all dbs + gene
-                platform, type="categorical")$Title, silent = silent))
-        } else if (is.character(databases)) {
-            dbs <- getDBs(databases, platform = platform, silent = silent)
-        } else {
-            dbs <- databases
-        }
-
-        if (include_genes) {
-            dbs <- c(dbs, buildGeneDBs(query, platform, silent = silent))
-        }
-
-        ## there shouldn't be empty databases, but just in case
-        dbs <- dbs[vapply(dbs, length, integer(1)) > 0]
-        if (!silent) {
-            message(sprintf("Testing against %d database(s)...", length(dbs)))
-        }
-
-        if (is.null(universe)) {
-            universe <- sesameDataGet(paste0(
-                platform, ".address"))$ordering$Probe_ID
-        } else { # subset the dbs by universe
-            dbs <- subsetDBs(dbs, universe) }
-
-        res <- do.call(bind_rows, lapply(dbs, function(db) {
-            testEnrichmentFisher(query = query, database = db,
-                universe = universe, alternative = alternative)}))
-        rownames(res) <- NULL
-        ## bind meta data
-        res <- cbind(res, databases_getMeta(dbs))
+        ## Array-based enrichment testing
+        res <- perform_array_enrichment(
+            query = query,
+            databases = databases,
+            universe = universe,
+            alternative = alternative,
+            include_genes = include_genes,
+            platform = platform,
+            silent = silent
+        )
     }
-
+    
+    ## Apply multiple testing correction
     res <- set_FDR(res, mtc_by_group = mtc_by_group, mtc_method = mtc_method)
+    
+    ## Order results by significance
     res[order(res$log10.p.value, -abs(res$estimate)), ]
 }
 
-set_FDR <- function(res, mtc_by_group = TRUE, mtc_method = "fdr") {
-    if (mtc_by_group) {
-        if (!is.null(res$group)) { # array data
-            group <- res$group
-        } else if (!is.null(res$MFile)) { # sequencing data don't have group
-            group <- res$MFile
-        } else {
-            stop("Cannot adjust p-values by group.")
-        }
-        grp_ind <- split(seq_len(nrow(res)), group)
-        grp_fdr <- do.call(c,lapply(grp_ind,function(x) {
-            p.adjust(res$p.value[x],method = mtc_method)
-        }))
-        res$FDR[unlist(grp_ind)] <- unname(grp_fdr)
-    } else {
-        res$FDR <- p.adjust(res$p.value, method = mtc_method)
+## Helper function to perform array-based enrichment testing
+perform_array_enrichment <- function(
+    query, databases, universe, alternative, 
+    include_genes, platform, silent) {
+    
+    ## Validate platform
+    platform <- queryCheckPlatform(platform, query, silent = silent)
+    
+    ## Get databases
+    dbs <- get_databases(databases, platform, silent)
+    
+    ## Add gene databases if requested
+    if (include_genes) {
+        gene_dbs <- buildGeneDBs(query, platform, silent = silent)
+        dbs <- c(dbs, gene_dbs)
     }
+    
+    ## Remove empty databases
+    dbs <- Filter(function(x) length(x) > 0, dbs)
+    
+    if (!silent) {
+        message(sprintf("Testing against %d database(s)...", length(dbs)))
+    }
+    
+    ## Get or validate universe
+    universe <- get_universe(universe, platform, dbs)
+    
+    ## Perform enrichment tests
+    res_list <- lapply(dbs, function(db) {
+        nD <- length(db)
+        nQ <- length(query)
+        nDQ <- length(intersect(query, db))
+        nU <- length(universe)
+        
+        testEnrichmentFisherN(nD, nQ, nDQ, nU, alternative = alternative)
+    })
+    
+    ## Combine results
+    res <- dplyr::bind_rows(res_list)
+    rownames(res) <- NULL
+    
+    ## Add metadata
+    cbind(res, databases_getMeta(dbs))
+}
+
+## Helper function to get databases
+get_databases <- function(databases, platform, silent) {
+    if (is.null(databases)) {
+        ## Get all available databases by default
+        db_groups <- listDBGroups(platform, type = "categorical")$Title
+        getDBs(db_groups, silent = silent)
+    } else if (is.character(databases)) {
+        getDBs(databases, platform = platform, silent = silent)
+    } else {
+        databases
+    }
+}
+
+## Helper function to get or validate universe
+get_universe <- function(universe, platform, dbs) {
+    if (is.null(universe)) {
+        address_data <- sesameDataGet(paste0(platform, ".address"))
+        address_data$ordering$Probe_ID
+    } else {
+        ## Subset databases by universe
+        subsetDBs(dbs, universe)
+        universe
+    }
+}
+
+## Apply FDR correction to enrichment results
+set_FDR <- function(res, mtc_by_group = TRUE, mtc_method = "fdr") {
+    
+    if (!mtc_by_group) {
+        res$FDR <- p.adjust(res$p.value, method = mtc_method)
+        return(res)
+    }
+    
+    ## Determine grouping variable
+    group <- determine_group(res)
+    
+    ## Calculate FDR by group
+    grp_ind <- split(seq_len(nrow(res)), group)
+    grp_fdr <- lapply(grp_ind, function(idx) {
+        p.adjust(res$p.value[idx], method = mtc_method)
+    })
+    
+    ## Assign FDR values back to results
+    res$FDR[unlist(grp_ind)] <- unname(unlist(grp_fdr))
     res
 }
 
-#' testEnrichmentFisher uses Fisher's exact test to estimate the association
-#' between two categorical variables.
-#'
-#' Estimates log2 Odds ratio
-#'
-#' @param query Vector of probes of interest (e.g., significant probes)
-#' @param database Vectors corresponding to the database set of
-#' interest with associated meta data as an attribute to each element.
-#' @param universe Vector of probes in the universe set containing all of
-#' @param alternative greater or two.sided (default: greater)
-#' the probes to be considered in the test. (Default: NULL)
-#'
-#' @import stats
-#'
-#' @return A DataFrame with the estimate/statistic, p-value, and name of test
-#' for the given results.
-testEnrichmentFisher <- function(query, database, universe,
-    alternative = "greater") {
-
-    nD <- length(database)
-    nQ <- length(query)
-    nDQ <- length(intersect(query, database))
-    nU <- length(universe)
-
-    testEnrichmentFisherN(nD, nQ, nDQ, nU, alternative = alternative)
-}    
-
-testEnrichmentFisherN <- function(
-    nD, nQ, nDQ, nU, alternative = "greater") {
-
-    nDmQ <- nD - nDQ
-    nQmD <- nQ - nDQ
-    nUmDQ <- nU - nQ - nD + nDQ
-
-    if (alternative == "two.sided") {
-        pvg <- phyper(
-            nDQ-1, nDQ + nQmD, nUmDQ + nDmQ, nDmQ + nDQ,
-            lower.tail = FALSE, log.p = TRUE) / log(10)
-        pvl <- phyper(
-            nDQ, nDQ + nQmD, nUmDQ + nDmQ, nDmQ + nDQ,
-            lower.tail = TRUE, log.p = TRUE) / log(10)
-        log10.p.value <- pmin(pmin(pvg, pvl) + log10(2), 0)
-    } else if (alternative == "greater") {
-        log10.p.value <- phyper(
-            nDQ-1, nDQ + nQmD, nUmDQ + nDmQ, nDmQ + nDQ,
-            lower.tail = FALSE, log.p = TRUE) / log(10)
-    } else if (alternative == "less") {
-        log10.p.value <- phyper(
-            nDQ, nDQ + nQmD, nUmDQ + nDmQ, nDmQ + nDQ,
-            lower.tail = TRUE, log.p = TRUE) / log(10)
+## Determine grouping variable for FDR correction
+determine_group <- function(res) {
+    if (!is.null(res$group)) {
+        ## Array data
+        res$group
+    } else if (!is.null(res$MFile)) {
+        ## Sequencing data
+        res$MFile
     } else {
-        stop("alternative must be either greater, less or two-sided.")
+        stop("Cannot adjust p-values by group: no 'group' or 'MFile' column found.")
     }
-
-    odds_ratio <- nDQ / nQmD / nDmQ * nUmDQ # can be NaN if 0
-    odds_ratio[odds_ratio == Inf] <- .Machine$double.xmax
-    odds_ratio[odds_ratio == 0] <- .Machine$double.xmin
-    data.frame(
-        estimate = log2(odds_ratio),
-        p.value = 10**(log10.p.value),
-        log10.p.value = log10.p.value,
-        test = "Log2(OR)",
-        nU = nU, nQ = nQ, nD = nD, overlap = nDQ,
-        cf_Jaccard = nDQ / (nD + nQmD),
-        cf_MCC = (as.numeric(nDQ) * as.numeric(nUmDQ) - as.numeric(nQmD)
-            * as.numeric(nDmQ))/sqrt(as.numeric(nD) * (nU - nD) * nQ * (nU - nQ)),
-        cf_overlap = nDQ / pmin(nD, nQ), # Szymkiewicz–Simpson
-        cf_NPMI = (log2(nD)+log2(nQ)-2*log2(nU))/(log2(nDQ)-log2(nU))-1,
-        cf_SorensenDice = 2 * nDQ/(nD + nQ))
 }
 
+## Calculate Fisher's exact test from counts
+testEnrichmentFisherN <- function(
+    nD, nQ, nDQ, nU, alternative = "greater") {
+    
+    ## Calculate contingency table values
+    nDmQ <- nD - nDQ      ## In database but not query
+    nQmD <- nQ - nDQ      ## In query but not database
+    nUmDQ <- nU - nQ - nD + nDQ  ## In neither
+    
+    ## Calculate p-value based on alternative hypothesis
+    log10.p.value <- calculate_fisher_pvalue(
+        nDQ, nQmD, nUmDQ, nDmQ, alternative
+    )
+    
+    ## Calculate odds ratio with safeguards
+    odds_ratio <- calculate_odds_ratio(nDQ, nQmD, nDmQ, nUmDQ)
+    
+    ## Calculate additional effect size metrics
+    cf_metrics <- calculate_effect_sizes(nD, nQ, nDQ, nU, nQmD, nDmQ, nUmDQ)
+    
+    data.frame(
+        estimate = log2(odds_ratio),
+        p.value = 10^log10.p.value,
+        log10.p.value = log10.p.value,
+        test = "Log2(OR)",
+        nU = nU, 
+        nQ = nQ, 
+        nD = nD, 
+        overlap = nDQ,
+        cf_Jaccard = cf_metrics$jaccard,
+        cf_MCC = cf_metrics$mcc,
+        cf_overlap = cf_metrics$overlap,
+        cf_NPMI = cf_metrics$npmi,
+        cf_SorensenDice = cf_metrics$sorensen_dice
+    )
+}
+
+## Calculate Fisher's exact test p-value
+calculate_fisher_pvalue <- function(nDQ, nQmD, nUmDQ, nDmQ, alternative) {
+    m <- nDQ + nQmD
+    n <- nUmDQ + nDmQ
+    k <- nDmQ + nDQ
+    
+    if (alternative == "two.sided") {
+        pvg <- phyper(nDQ - 1, m, n, k, lower.tail = FALSE, log.p = TRUE) / log(10)
+        pvl <- phyper(nDQ, m, n, k, lower.tail = TRUE, log.p = TRUE) / log(10)
+        pmin(pmin(pvg, pvl) + log10(2), 0)
+    } else if (alternative == "greater") {
+        phyper(nDQ - 1, m, n, k, lower.tail = FALSE, log.p = TRUE) / log(10)
+    } else if (alternative == "less") {
+        phyper(nDQ, m, n, k, lower.tail = TRUE, log.p = TRUE) / log(10)
+    } else {
+        stop("alternative must be 'greater', 'less', or 'two.sided'.")
+    }
+}
+
+## Calculate odds ratio with safeguards for extreme values
+calculate_odds_ratio <- function(nDQ, nQmD, nDmQ, nUmDQ) {
+    odds_ratio <- (nDQ * nUmDQ) / (nQmD * nDmQ)
+    
+    ## Handle extreme values
+    odds_ratio[is.infinite(odds_ratio)] <- .Machine$double.xmax
+    odds_ratio[odds_ratio == 0] <- .Machine$double.xmin
+    odds_ratio[is.nan(odds_ratio)] <- NA_real_
+    
+    odds_ratio
+}
+
+## Calculate effect size metrics
+calculate_effect_sizes <- function(nD, nQ, nDQ, nU, nQmD, nDmQ, nUmDQ) {
+    list(
+        jaccard = nDQ / (nD + nQmD),
+        mcc = calculate_mcc(nDQ, nUmDQ, nQmD, nDmQ, nD, nU, nQ),
+        overlap = nDQ / pmin(nD, nQ),  ## Szymkiewicz-Simpson coefficient
+        npmi = calculate_npmi(nD, nQ, nDQ, nU),
+        sorensen_dice = (2 * nDQ) / (nD + nQ)
+    )
+}
+
+## Calculate Matthews correlation coefficient
+calculate_mcc <- function(nDQ, nUmDQ, nQmD, nDmQ, nD, nU, nQ) {
+    numerator <- as.numeric(nDQ) * as.numeric(nUmDQ) - 
+                 as.numeric(nQmD) * as.numeric(nDmQ)
+    denominator <- sqrt(
+        as.numeric(nD) * (nU - nD) * nQ * (nU - nQ)
+    )
+    numerator / denominator
+}
+
+## Calculate Normalized Pointwise Mutual Information
+calculate_npmi <- function(nD, nQ, nDQ, nU) {
+    (log2(nD) + log2(nQ) - 2 * log2(nU)) / 
+    (log2(nDQ) - log2(nU)) - 1
+}
--- R/testEnrichment2.R
+++ R/testEnrichment2.R
@@ -1,37 +1,238 @@
-
 #' Test enrichment from YAME-compressed CG sets
 #' 
-#' @param query_fn File path to query
-#' @param knowledge_fn File path to knowledgebase
-#' @param universe_fn optional file path to universe
-#' @param alternative greater, less
-#' @return a tibble
+#' Tests for enrichment of genomic regions in YAME-compressed CG sets using
+#' Fisher's exact test. This function is optimized for sequencing data and
+#' uses compiled C code for efficient processing.
+#' 
+#' @param query_fn Character string specifying the file path to the query
+#' CG set file (YAME-compressed format).
+#' @param knowledge_fn Character string or vector specifying the file path(s)
+#' to the knowledgebase file(s) (YAME-compressed format). Can be a single
+#' file or multiple files.
+#' @param universe_fn Optional character string specifying the file path to
+#' the universe CG set file. If NULL, universe will be inferred from the
+#' knowledgebase. (Default: NULL)
+#' @param alternative Character string specifying the alternative hypothesis:
+#' "greater" (enrichment), "less" (depletion), or "two.sided". 
+#' (Default: "greater")
+#' @param min_overlap Minimum number of overlapping CGs required for a test
+#' to be included in results. (Default: 1)
+#' @param verbose Logical indicating whether to print progress messages.
+#' (Default: FALSE)
+#' 
+#' @return A tibble containing enrichment test results with the following columns:
+#' \describe{
+#'   \item{Mask}{Name/identifier of the knowledgebase mask}
+#'   \item{N_mask}{Number of CGs in the mask}
+#'   \item{N_query}{Number of CGs in the query}
+#'   \item{N_overlap}{Number of overlapping CGs}
+#'   \item{N_univ}{Total number of CGs in universe}
+#'   \item{estimate}{Log2 odds ratio}
+#'   \item{p.value}{P-value from Fisher's exact test}
+#'   \item{log10.p.value}{Log10-transformed p-value}
+#'   \item{test}{Type of test performed}
+#'   \item{Additional effect size metrics}{Jaccard, MCC, etc.}
+#' }
+#' 
 #' @useDynLib knowYourCG, .registration = TRUE
+#' @importFrom tibble as_tibble
+#' @importFrom utils read.table
+#' 
 #' @export
+#' 
 #' @examples
-#' if (.Platform$OS.type!="windows") {
-#' kfn = system.file("extdata", "chromhmm.cm", package = "knowYourCG")
-#' qfn = system.file("extdata", "onecell.cg", package = "knowYourCG")
-#' testEnrichment2(qfn, kfn)
+#' if (.Platform$OS.type != "windows") {
+#'   kfn <- system.file("extdata", "chromhmm.cm", package = "knowYourCG")
+#'   qfn <- system.file("extdata", "onecell.cg", package = "knowYourCG")
+#'   res <- testEnrichment2(qfn, kfn)
+#'   head(res)
 #' }
 testEnrichment2 <- function(
-    query_fn, knowledge_fn, universe_fn=NULL, alternative="greater") {
-
-    stopifnot(is.character(query_fn), is.character(knowledge_fn))
+    query_fn, 
+    knowledge_fn, 
+    universe_fn = NULL, 
+    alternative = "greater",
+    min_overlap = 1,
+    verbose = FALSE) {
+    
+    ## Validate inputs
+    validate_inputs(query_fn, knowledge_fn, universe_fn)
+    
+    ## Check platform compatibility
     if (.Platform$OS.type == "windows") {
-        stop("Testing sequencing data does not support Windows.")
+        stop(
+            "Testing sequencing data is not currently supported on Windows. ",
+            "This feature requires compiled C code that is not available on Windows.",
+            call. = FALSE
+        )
     }
-    yame_result <- .Call("yame_summary_cfunc", query_fn, knowledge_fn)
     
-    ## args <- c("summary", "-m", knowledgebase_fn, query_fn)
-    ## yame_result <- system2("yame", args, stdout = TRUE, stderr = TRUE)
+    ## Validate alternative parameter
+    alternative <- match.arg(alternative, c("greater", "less", "two.sided"))
     
-    ## command <- paste("yame summary -m", knowledgebase_fn, query_fn)
-    ## yame_result <- system(command, intern = TRUE)
-    df <- tibble::as_tibble(read.table(
-        text = paste(yame_result, collapse = "\n"), header = TRUE))
-    res <- cbind(df, testEnrichmentFisherN(
-        nD = df$N_mask, nQ = df$N_query, nDQ = df$N_overlap, nU = df$N_univ))
-    res <- res[!is.na(res$Mask),]
+    if (verbose) {
+        message("Reading query file: ", query_fn)
+        message("Reading knowledgebase file(s): ", 
+                paste(knowledge_fn, collapse = ", "))
+    }
+    
+    ## Call compiled C function to compute overlaps
+    yame_result <- tryCatch(
+        .Call("yame_summary_cfunc", query_fn, knowledge_fn),
+        error = function(e) {
+            stop(
+                "Failed to process YAME files: ", e$message,
+                "\nPlease ensure files are in valid YAME-compressed format.",
+                call. = FALSE
+            )
+        }
+    )
+    
+    ## Parse results into data frame
+    df <- parse_yame_results(yame_result)
+    
+    if (nrow(df) == 0) {
+        warning("No results returned from YAME processing.", call. = FALSE)
+        return(tibble::tibble())
+    }
+    
+    if (verbose) {
+        message(sprintf("Processing %d mask(s)...", nrow(df)))
+    }
+    
+    ## Perform Fisher's exact test for each mask
+    res <- compute_enrichment_stats(df, alternative, min_overlap)
+    
+    ## Remove rows with missing mask names
+    res <- res[!is.na(res$Mask) & res$Mask != "", ]
+    
+    if (nrow(res) == 0) {
+        warning("No valid results after filtering.", call. = FALSE)
+        return(tibble::tibble())
+    }
+    
+    ## Order by significance
+    res <- res[order(res$log10.p.value, -abs(res$estimate)), ]
+    
+    if (verbose) {
+        message(sprintf(
+            "Completed enrichment testing for %d mask(s).", 
+            nrow(res)
+        ))
+    }
+    
     res
+}
+
+## Validate input file paths
+validate_inputs <- function(query_fn, knowledge_fn, universe_fn) {
+    ## Check input types
+    if (!is.character(query_fn) || length(query_fn) != 1) {
+        stop("'query_fn' must be a single character string.", call. = FALSE)
+    }
+    
+    if (!is.character(knowledge_fn)) {
+        stop("'knowledge_fn' must be a character string or vector.", call. = FALSE)
+    }
+    
+    if (!is.null(universe_fn) && !is.character(universe_fn)) {
+        stop("'universe_fn' must be NULL or a character string.", call. = FALSE)
+    }
+    
+    ## Check file existence
+    if (!file.exists(query_fn)) {
+        stop("Query file not found: ", query_fn, call. = FALSE)
+    }
+    
+    missing_kb <- knowledge_fn[!file.exists(knowledge_fn)]
+    if (length(missing_kb) > 0) {
+        stop(
+            "Knowledgebase file(s) not found: ",
+            paste(missing_kb, collapse = ", "),
+            call. = FALSE
+        )
+    }
+    
+    if (!is.null(universe_fn) && !file.exists(universe_fn)) {
+        stop("Universe file not found: ", universe_fn, call. = FALSE)
+    }
+}
+
+## Parse YAME results into a tibble
+parse_yame_results <- function(yame_result) {
+    if (is.null(yame_result) || length(yame_result) == 0) {
+        stop("Empty result from YAME processing.", call. = FALSE)
+    }
+    
+    ## Convert to tibble
+    df <- tryCatch(
+    {
+        text_data <- paste(yame_result, collapse = "\n")
+        temp_df <- read.table(
+            text = text_data, 
+            header = TRUE,
+            stringsAsFactors = FALSE,
+            comment.char = ""
+        )
+        tibble::as_tibble(temp_df)
+    },
+    error = function(e) {
+        stop(
+            "Failed to parse YAME output: ", e$message,
+            "\nOutput may be malformed.",
+            call. = FALSE
+        )
+    }
+    )
+    
+    ## Validate expected columns
+    required_cols <- c("Mask", "N_mask", "N_query", "N_overlap", "N_univ")
+    missing_cols <- setdiff(required_cols, names(df))
+    
+    if (length(missing_cols) > 0) {
+        stop(
+            "YAME output missing required columns: ",
+            paste(missing_cols, collapse = ", "),
+            call. = FALSE
+        )
+    }
+    
+    df
+}
+
+## Compute enrichment statistics for each mask
+compute_enrichment_stats <- function(df, alternative, min_overlap) {
+    ## Filter by minimum overlap if specified
+    if (min_overlap > 1) {
+        df <- df[df$N_overlap >= min_overlap, ]
+    }
+    
+    if (nrow(df) == 0) {
+        warning(
+            "No masks meet minimum overlap threshold (", 
+            min_overlap, ").",
+            call. = FALSE
+        )
+        return(tibble::tibble())
+    }
+    
+    ## Available from the main enrichment file
+    ## It calculates Fisher's exact test statistics from count data
+    enrichment_stats <- testEnrichmentFisherN(
+        nD = df$N_mask,
+        nQ = df$N_query,
+        nDQ = df$N_overlap,
+        nU = df$N_univ,
+        alternative = alternative
+    )
+    
+    ## Combine with original data
+    res <- cbind(df, enrichment_stats)
+    
+    ## Add MFile column to track source if needed
+    if (!"MFile" %in% names(res)) {
+        res$MFile <- "YAME"
+    }
+    
+    tibble::as_tibble(res)
 }
